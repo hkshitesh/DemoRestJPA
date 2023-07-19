b24f85cc6d938ad9352c0d395ae382083f471217
Dr. Hitesh Kumar Sharma <hkshitesh@gmail.com>
2023-07-19 17:03:35 +0530

    pom editied

--- src/main/java/com/example/demo/service/EmployeeService.java
+++ src/main/java/com/example/demo/service/EmployeeService.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
 
 import com.example.demo.entity.Employee;
@@ -39,8 +40,10 @@ public class EmployeeService {
 		return repository.save(existingEmployee);
 	}
 	public String deleteEMp(int id) {
-		repository.deleteById(id);
+		repository.deleteById(id);		
 		return "Employee "+id+" Deleted";
 	}
+	
+	
 
 }
